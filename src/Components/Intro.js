a5cf1fb51b747f36eac112a90cdae15ee2da3ce1
DvirK95 <dvirkarl@gmail.com>
2023-04-27 08:30:35 +0300

    27/04/2023

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -1,13 +1,13 @@
-import "./Intro.css";
-import { Row } from "react-bootstrap";
-import profileImg from "../photos/DvirProfile.png";
-import Social from "./Social";
+import './Intro.css';
+import { Row } from 'react-bootstrap';
+import profileImg from '../photos/DvirProfile.png';
+import Social from './Social';
 
 function Intro() {
   return (
     <section id="intro">
       <div className="infos">
-        <h1 className="title">Full Stack Developer and & Automation</h1>
+        <h1 className="title">Developer and & Automation</h1>
         <p>Passionate about coding and always eager to learn technologies</p>
         <Row className="justify-content-center">
           <Social />
